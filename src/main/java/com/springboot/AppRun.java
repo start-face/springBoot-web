0a6c357bf23cd0bb9d9e6d5432b4de025f5a176f
tcaironglnan <995956062@qq.com>
2019-02-14 10:58:55 +0800

    临时提交

--- src/main/java/com/springboot/AppRun.java
+++ src/main/java/com/springboot/AppRun.java
@@ -2,17 +2,18 @@ package com.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author FaceFeel
  * @Created 2018-04-17 11:23
  **/
-
 @SpringBootApplication
+//指定扫描包路径
+@ComponentScan(basePackages = {"com.springboot.*"})
 public class AppRun {
 
     public static void main(String[] args) {
-
         //项目启动:mvn spring-boot:run
         SpringApplication.run(AppRun.class,args);
     }
